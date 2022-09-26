3c679b3b3114e26434461dca85a9a996162b2ba2
mkankit12 <ankit.maurya@edeltaes.com>
2022-09-26 18:32:13 +0530

    Initial commit

---
+++ src/utils/type.ts
@@ -0,0 +1,15 @@
+export class createUserParams {
+  username: string;
+  password: string;
+}
+export class updateUserParams {
+  username: string;
+  password: string;
+}
+
+export class createuserProfileParams {
+  firstName: string;
+  lastName: string;
+  age: number;
+  dob: string;
+}
